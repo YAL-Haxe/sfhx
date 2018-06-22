3b7ee8e82611e3a1f082f23e39e7b855f95f9e2e
YellowAfterlife <yellowafterlife@ymail.com>
2018-06-22 10:49:13 +0300

    Removed C# version from SfEnumTools as that doesn't work.

--- SfEnumTools.hx
+++ SfEnumTools.hx
@@ -32,40 +32,12 @@ private typedef SfEnumValue = {
 	tag: Dynamic,
 	index: Int
 }
-#elseif (cs)
-class SfEnumTools {
-	private static inline function asValue<T>(q:EnumValue):cs.internal.HxObject.ParamEnum {
-		return cast q;
-	}
-	public static inline function getParameter<T>(q:EnumValue, i:Int):Any {
-		return asValue(q).params[i];
-	}
-	public static inline function setParameter<T>(q:EnumValue, i:Int, v:Any):Void {
-		asValue(q).params[i] = v;
-	}
-	public static inline function getParameterCount<T>(q:EnumValue):Int {
-		return asValue(q).params.length;
-	}
-	public static function setTo<T:EnumValue>(q:T, v:T):Void {
-		var qi = asValue(q);
-		var vi = asValue(v);
-		var vx = vi.params;
-		if (vx != null) {
-			qi.params = vx.copy();
-		} else qi.params = null;
-		qi.index = vi.index;
-	}
-}
-//private typedef SfEnumValue = cs.internal.HxObject.ParamEnum;
-/*private interface SfEnumValue {
-	public var params:haxe.ds.Vector<Dynamic>;
-	public var index:Int;
-}*/
 #else
 class SfEnumTools {
 	//private static inline function asValue<T>(q:EnumValue):SfEnumValue return cast q;
 	public static inline function getParameter<T>(q:EnumValue, i:Int):Any {
-		throw "Can't getParameter from " + q;
+		return (q.getParameters())[i];
+		//throw "Can't getParameter from " + q;
 		//return asValue(q).args[i];
 	}
 	public static inline function setParameter<T>(q:EnumValue, i:Int, v:Any):Void {
