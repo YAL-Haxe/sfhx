5d1ddcda2bfc4aa01509d4f76ca211744d07e12c
YellowAfterlife <yellowafterlife@ymail.com>
2018-10-07 03:26:55 +0300

    Maintain a map of anon type fields

--- sf/type/SfAnonImpl.hx
+++ sf/type/SfAnonImpl.hx
@@ -1,4 +1,5 @@
 package sf.type;
+import haxe.ds.Map;
 import haxe.macro.Context;
 import haxe.macro.Type;
 
@@ -10,12 +11,15 @@ class SfAnonImpl extends SfType {
 	public var defType:DefType;
 	public var anonType:AnonType;
 	public var fields:Array<SfClassField> = [];
+	public var fieldMap:Map<String, SfClassField> = new Map();
 	public function new(t:DefType, at:AnonType) {
 		super(t);
 		defType = t;
 		anonType = at;
 		for (f in at.fields) {
-			fields.push(new SfClassField(this, f, true));
+			var sf = new SfClassField(this, f, true);
+			fields.push(sf);
+			fieldMap.set(sf.name, sf);
 		}
 		fields.sort(fieldSort);
 	}
