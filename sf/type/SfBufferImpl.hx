2e3d79e9e2207850bf0164e85af876362c623fcd
YellowAfterlife <yellowafterlife@ymail.com>
2019-04-04 15:13:07 +0300

    Let the compiler figure out what to inline

--- sf/type/SfBufferImpl.hx
+++ sf/type/SfBufferImpl.hx
@@ -26,44 +26,44 @@ class SfBufferImpl extends SfBufferBase {
 	public function new() {
 		super();
 	}
-	public inline function addString(s:String) this.add(s);
-	public inline function addInt(i:Int) this.add(i);
-	public inline function addFloat(f:Float) this.add(f);
-	public inline function addBuffer(b:SfBufferImpl) this.add(b);
+	public function addString(s:String) this.add(s);
+	public function addInt(i:Int) this.add(i);
+	public function addFloat(f:Float) this.add(f);
+	public function addBuffer(b:SfBufferImpl) this.add(b);
 	
 	/** A shortcut to add two characters at once. */
-	public inline function addChar2(c1:Int, c2:Int) {
+	public function addChar2(c1:Int, c2:Int) {
 		addChar(c1);
 		addChar(c2);
 	}
 	
 	/** A shortcut to add 3 characters at once. */
-	public inline function addChar3(c1:Int, c2:Int, c3:Int) {
+	public function addChar3(c1:Int, c2:Int, c3:Int) {
 		addChar(c1);
 		addChar(c2);
 		addChar(c3);
 	}
 	
 	/** A shortcut for adding a semicolon */
-	public inline function addSemico() {
+	public function addSemico() {
 		if (length > semicoAfter) {
 			addChar(";".code);
 			semicoAfter = length;
 		}
 	}
 	
-	public inline function markSemico() {
+	public function markSemico() {
 		semicoAfter = length;
 	}
 	
 	/** A shortcut for a comma + optional space */
-	public inline function addComma() {
+	public function addComma() {
 		addChar(",".code);
 		addSep();
 	}
 	
 	/** A shortcut for dot\period */
-	public inline function addDot() addChar(".".code);
+	public function addDot() addChar(".".code);
 	
 	public function addTypePath(s:SfType, sep:Int = ".".code) {
 		var p = s.pack;
@@ -98,14 +98,14 @@ class SfBufferImpl extends SfBufferBase {
 	}
 	
 	/** Adds an expression */
-	public inline function addExpr(e:SfExpr, wrap:Bool) {
+	public function addExpr(e:SfExpr, wrap:Bool) {
 		#if !display
 		sfGenerator.printExpr(cast this, e, wrap);
 		#end
 	}
 	
 	/** Adds an { expression } */
-	public inline function addBlockExpr(e:SfExpr) {
+	public function addBlockExpr(e:SfExpr) {
 		addBlockOpen();
 		if (SfExprTools.isEmpty(e)) {
 			addSep();
@@ -145,10 +145,10 @@ class SfBufferImpl extends SfBufferBase {
 	}
 	
 	/** A shortcut for "{" */
-	public inline function addBlockOpen() addChar("{".code);
+	public function addBlockOpen() addChar("{".code);
 	
 	/** A shortcut for "}" */
-	public inline function addBlockClose() {
+	public function addBlockClose() {
 		addChar("}".code);
 		semicoAfter = length;
 	}
@@ -156,19 +156,19 @@ class SfBufferImpl extends SfBufferBase {
 	//{ ()
 	
 	/** A shortcut for "(" */
-	public inline function addParOpen() addChar("(".code);
+	public function addParOpen() addChar("(".code);
 	
 	/** A shortcut for ")" */
-	public inline function addParClose() addChar(")".code);
+	public function addParClose() addChar(")".code);
 	
 	/** Adds */
-	public inline function addParExpr(e:SfExpr) {
+	public function addParExpr(e:SfExpr) {
 		addParOpen();
 		addExpr(e, true);
 		addParClose();
 	}
 	
-	public inline function addParExpr2(e1:SfExpr, e2:SfExpr) {
+	public function addParExpr2(e1:SfExpr, e2:SfExpr) {
 		addParOpen();
 		addExpr(e1, true);
 		addComma();
@@ -176,7 +176,7 @@ class SfBufferImpl extends SfBufferBase {
 		addParClose();
 	}
 	
-	public inline function addParExpr3(e1:SfExpr, e2:SfExpr, e3:SfExpr) {
+	public function addParExpr3(e1:SfExpr, e2:SfExpr, e3:SfExpr) {
 		addParOpen();
 		addExpr(e1, true);
 		addComma();
@@ -189,19 +189,19 @@ class SfBufferImpl extends SfBufferBase {
 	//{ sep
 	
 	/** Adds an optional separator */
-	public inline function addSep() {
+	public function addSep() {
 		if (sfConfig.pretty) addChar(" ".code);
 	}
 	
 	/** Adds a character, surrounded by separators */
-	public inline function addSepChar(c:Int) {
+	public function addSepChar(c:Int) {
 		addSep();
 		addChar(c);
 		addSep();
 	}
 	
 	/** Adds 2 characters, surrounded by separators */
-	public inline function addSepChar2(c1:Int, c2:Int) {
+	public function addSepChar2(c1:Int, c2:Int) {
 		addSep();
 		addChar(c1);
 		addChar(c2);
@@ -209,7 +209,7 @@ class SfBufferImpl extends SfBufferBase {
 	}
 	
 	/** Adds 3 characters, surrounded by separators */
-	public inline function addSepChar3(c1:Int, c2:Int, c3:Int) {
+	public function addSepChar3(c1:Int, c2:Int, c3:Int) {
 		addSep();
 		addChar(c1);
 		addChar(c2);
@@ -219,9 +219,9 @@ class SfBufferImpl extends SfBufferBase {
 	
 	//}
 	//{
-	public inline function addHintOpen() addChar3("/".code, "*".code, " ".code);
-	public inline function addHintClose() addChar3(" ".code, "*".code, "/".code);
-	public inline function addHintString(s:String) {
+	public function addHintOpen() addChar3("/".code, "*".code, " ".code);
+	public function addHintClose() addChar3(" ".code, "*".code, "/".code);
+	public function addHintString(s:String) {
 		if (sfConfig.hint) {
 			addHintOpen();
 			addString(s);
